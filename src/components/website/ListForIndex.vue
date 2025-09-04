15c70f1dea7697ba86a01add68ad0867353cd2e7
YuKi Yan <email>
2025-09-04 19:23:52 +0800

    无限加载优化

--- src/components/website/ListForIndex.vue
+++ src/components/website/ListForIndex.vue
@@ -1,36 +1,37 @@
 <template>
     <div class="w-index-list">
-
-        <ul class="infinite-list" v-infinite-scroll="load" style="overflow:auto">
-            <li>
-                <el-carousel :interval="5000">
-                    <el-carousel-item v-for="(item, index) in imgGroup" :key="index">
-                        <img :src="item" alt="">
-                    </el-carousel-item>
-                </el-carousel>
-            </li>
-            <li v-for="(i, k) in dataList" class="infinite-list-item" :key="k">
-                <img :src="require(`@/assets/img/${i.src}`)" @click="toDetail(i.id)" alt="">
-                <div class="list-content">
-                    <div class="list-info">
-                        <div class="list-info-child list-title">{{ i.title }}</div>
-                        <div class="list-info-child list-author-info">
-                            <span class="list-author">{{ i.author }}</span>
-                            <span>{{ i.date }}</span>
+        <div class="list-container" v-infinite-scroll="load" style="overflow:auto">
+            <ul class="infinite-list">
+                <li>
+                    <el-carousel :interval="5000">
+                        <el-carousel-item v-for="(item, index) in imgGroup" :key="index">
+                            <img :src="item" alt="">
+                        </el-carousel-item>
+                    </el-carousel>
+                </li>
+                <li v-for="(i, k) in dataList" class="infinite-list-item" :key="k">
+                    <img :src="require(`@/assets/img/${i.src}`)" @click="toDetail(i.id)" alt="">
+                    <div class="list-content">
+                        <div class="list-info">
+                            <div class="list-info-child list-title">{{ i.title }}</div>
+                            <div class="list-info-child list-author-info">
+                                <span class="list-author">{{ i.author }}</span>
+                                <span>{{ i.date }}</span>
+                            </div>
+                            <div class="list-info-child list-remark">{{ i.remark }}</div>
                         </div>
-                        <div class="list-info-child list-remark">{{ i.remark }}</div>
+                        <div class="list-option">
+                            <div class="list-price">¥{{ i.price }}</div>
+                            <el-button type="primary" @click="addShopCar(i.id)">
+                                加入购物车
+                            </el-button>
+                        </div>
                     </div>
-                    <div class="list-option">
-                        <div class="list-price">¥{{ i.price }}</div>
-                        <el-button type="primary" @click="addShopCar(i.id)">
-                            加入购物车
-                        </el-button>
-                    </div>
-                </div>
-            </li>
-            <p v-if="loading">加载中...</p>
-            <p v-if="noMore">没有更多了</p>
-        </ul>
+                </li>
+                <p v-if="loading">加载中...</p>
+                <p v-if="noMore">没有更多了</p>
+            </ul>
+        </div>
     </div>
 </template>
 <script>
@@ -76,7 +77,6 @@ export default {
                     this.count += 2;
                     this.getData();
                 }, 2000);
-
             }
         },
         getData() {
@@ -92,7 +92,6 @@ export default {
                 this.$message.error(e);
             })
         },
-
     }
 }
 </script>
@@ -102,7 +101,8 @@ export default {
 
     .el-carousel {
         height: 320px;
-        .el-carousel__container{
+
+        .el-carousel__container {
             height: 100%;
         }
     }
@@ -113,109 +113,119 @@ export default {
         margin: 0;
     }
 
-    .infinite-list {
-        height: 800px;
-        padding-bottom: 20px;
-        .infinite-list-item {
-            margin: 20px 8px;
-            padding: 15px 12px;
-            display: flex;
-            flex-direction: row;
-            justify-content: flex-start;
-            border: 1px solid #e2b0a9;
-            border-radius: 3px;
-            cursor: pointer;
+    .list-container {
+        .infinite-list {
+            padding-bottom: 20px;
 
-            img {
-                width: 120px;
-                height: 120px;
-            }
-
-            .list-content {
-                width: calc(100% - 156px);
+            .infinite-list-item {
+                margin: 20px 8px;
+                padding: 15px 12px;
                 display: flex;
                 flex-direction: row;
-                justify-content: space-between;
-                align-items: flex-start;
-                padding: 3px 0px 3px 20px;
-                text-align: left;
+                justify-content: flex-start;
+                border: 1px solid #e2b0a9;
+                border-radius: 3px;
+                cursor: pointer;
 
-                .list-info {
-                    .list-info-child {
-                        margin-bottom: 12px;
-                        color: #808080;
+                img {
+                    width: 120px;
+                    height: 120px;
+                }
+
+                .list-content {
+                    width: calc(100% - 156px);
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: space-between;
+                    align-items: flex-start;
+                    padding: 3px 0px 3px 20px;
+                    text-align: left;
+
+                    .list-info {
+                        .list-info-child {
+                            margin-bottom: 12px;
+                            color: #808080;
+                        }
+
+                        .list-title {
+                            font-size: 14px;
+                            color: #000;
+                            width: 200px;
+                            overflow: hidden;
+                            text-overflow: ellipsis;
+                            white-space: nowrap;
+                        }
+
+                        .list-author-info {
+                            display: flex;
+                            justify-content: flex-start;
+                            align-items: center;
+
+                            .list-author {
+                                margin-right: 10px;
+                            }
+                        }
                     }
 
-                    .list-title {
-                        font-size: 14px;
-                        color: #000;
-                        width: 200px;
-                        overflow: hidden;
-                        text-overflow: ellipsis;
-                        white-space: nowrap;
-                    }
-
-                    .list-author-info {
+                    .list-option {
+                        height: 100%;
                         display: flex;
-                        justify-content: flex-start;
-                        align-items: center;
+                        flex-direction: column;
+                        justify-content: space-between;
+                        align-items: flex-end;
 
-                        .list-author {
-                            margin-right: 10px;
+                        .list-price {
+                            font-size: 20px;
+                            color: red;
+                            text-align: right;
+                            width: 120px;
+                            overflow: hidden;
+                            text-overflow: ellipsis;
+                            white-space: nowrap;
+                        }
+
+                        .el-button {
+                            height: 30px;
+                            font-size: 12px;
+                            line-height: 30px;
+                            color: #FFF;
+                            background-color: #e72106;
+                            border-color: #e72106;
+                            padding: 0 20px;
+                            border-radius: 18px;
+                        }
+
+                        .el-button:hover,
+                        .el-button:focus {
+                            background-color: #e72106;
+                            border-color: #e72106;
+                            box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
                         }
                     }
                 }
-
-                .list-option {
-                    height: 100%;
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: space-between;
-                    align-items: flex-end;
-
-                    .list-price {
-                        font-size: 20px;
-                        color: red;
-                        text-align: right;
-                        width: 120px;
-                        overflow: hidden;
-                        text-overflow: ellipsis;
-                        white-space: nowrap;
-                    }
-
-                    .el-button {
-                        height: 30px;
-                        font-size: 12px;
-                        line-height: 30px;
-                        color: #FFF;
-                        background-color: #e72106;
-                        border-color: #e72106;
-                        padding: 0 20px;
-                        border-radius: 18px;
-                    }
-
-                    .el-button:hover,
-                    .el-button:focus {
-                        background-color: #e72106;
-                        border-color: #e72106;
-                        box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
-                    }
-                }
             }
-        }
 
-        .infinite-list-item:hover {
-            box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.2);
-        }
+            .infinite-list-item:hover {
+                box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.2);
+            }
 
-        p {
-            color: #b9b9b9;
+            p {
+                color: #b9b9b9;
+            }
         }
     }
 }
 
+::-webkit-scrollbar {
+    display: none;
+}
+
 @media (max-width: 600px) {
     .w-index-list {
+        .list-container {
+            height: calc(100vh - 120px);
+        }
+
         .el-carousel {
             width: 100vw;
             height: 30vh;
@@ -225,31 +235,31 @@ export default {
             }
         }
 
-        .infinite-list {
-            padding-bottom: 80px;
+        .list-container {
+            .infinite-list {
+                .infinite-list-item {
+                    align-items: center;
+                    padding: 8px;
+                    margin: 15px 12px;
 
-            .infinite-list-item {
-                align-items: center;
-                padding: 8px;
-                margin: 15px 12px;
+                    .list-content {
+                        flex-direction: column;
+                        align-items: flex-end;
 
-                .list-content {
-                    flex-direction: column;
-                    align-items: flex-end;
+                        .list-option {
+                            justify-content: space-between;
+                            flex-direction: row;
+                            align-items: center;
+                            width: 100%;
 
-                    .list-option {
-                        justify-content: space-between;
-                        flex-direction: row;
-                        align-items: center;
-                        width: 100%;
+                            .list-price {
+                                width: 20vw;
+                                text-align: left;
+                            }
 
-                        .list-price {
-                            width: 20vw;
-                            text-align: left;
-                        }
-
-                        .el-button {
-                            margin-left: 20px;
+                            .el-button {
+                                margin-left: 20px;
+                            }
                         }
                     }
                 }
